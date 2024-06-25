b243b00e883a6225bfec512eb555e20b1d83e6a1
crabcatcher0 <rautprashant103@gmail.com>
2024-06-25 15:38:38 +0545

    output formatted

--- func.py
+++ func.py
@@ -1,6 +1,8 @@
 ##password generator
+from colorama import Fore, Back, Style
 import random
 import re
+import os
 
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
@@ -8,12 +10,13 @@ digits = "0123456789"
 special_characters = "!@#$%^&*,"
 
 all_char = uppercase_letters + lowercase_letters + digits + special_characters
+    
 
 
 def create_pass(length):
     if length < 8:
-        print("Length should be more then 8..")
-        return "Password generation failed."
+        print(f"{Fore.RED}Length should be more then 8..")
+        return f"{Fore.RED}✖ Error: Password generation failed.{Style.RESET_ALL}"
     
     else:
         password = (
@@ -21,31 +24,28 @@ def create_pass(length):
             random.choice(uppercase_letters) +
             random.choice(digits) +
             random.choice(special_characters) +
-            "".join(random.choice(all_char) for _ in range(length - 3)) 
+            "".join(random.choice(all_char) for _ in range(length - 4)) 
             )
         
         password_list = list(password)
         random.shuffle(password_list)
         password = ''.join(password_list)
 
-        return f"This is your password: '{password}'"
-    
-   
-# def write_pass(password):
-#     with open("generated_pass.txt", 'w') as file:
-#         file = file.write(password)
+        return f"{password}"
 
 
-def manage_pass(site_name, username, password):
+def manage_pass(name, site_name, username, password):
     with open("credential.txt", 'w') as file:
+        file.write(f"Welcome! {name}. Your informations:\n\n")
         file.write(f"Site Name: '{site_name}'\n")
         file.write(f"Username or Email: '{username}'\n")
         file.write(f"Password: '{password}'\n")         
 
+
 def password_check(input_pass):
 
     if len(input_pass) < 8:
-        return f"Password Strerngth: Too Weak."
+        return f"{Fore.RED}{Style.BRIGHT}✖ Password Strerngth: Too Weak.{Style.RESET_ALL}"
     
     has_lowercase = re.search(r'[a-z]', input_pass)
     has_uppercase = re.search(r'[A-Z]', input_pass)
@@ -53,7 +53,7 @@ def password_check(input_pass):
     has_digit = re.search(r'\d', input_pass)
 
     if has_lowercase and has_uppercase and has_character and has_digit:
-        return "Password Strength: Strong."
+        return f"{Fore.GREEN}{Style.BRIGHT}✔ Password Strength: Strong.u'\u2713'{Style.RESET_ALL}"
     elif has_lowercase and has_digit:
         return "Password Strength: Medium."
     elif has_lowercase:
@@ -62,6 +62,13 @@ def password_check(input_pass):
         return "Password Strength: Weak (Missing lowercase letters, digits, or special characters)."
 
 
+def show_my_password():
 
-    
+    if not os.path.exists("credential.txt"):
+        print(f"{Fore.RED}{Style.BRIGHT}✖Please generate and store your detail.{Style.RESET_ALL}")
+    else:
+        with open("credential.txt", 'r') as file:
+            file = file.read()
+            print(f"{Fore.GREEN}{Style.BRIGHT}{file}{Style.RESET_ALL}")
         
+    
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import func
+from colorama import Fore, Back, Style
+
 
 print("********* Welcome to Password Manager *********")
 password = ''
@@ -6,43 +8,43 @@ password = ''
 while True:
 
     user_input = input(
-    "Options: \n"
+    f"{Fore.BLUE}{Style.BRIGHT}Options: \n"
     "'G' to generate random password\n"
     "'M' to manage your account and password (Password must be generate first)\n"
+    "'S' to view your account.\n"
     "'C' to check your password strength\n"
     "'Q' to quit\n"
-    "Choose from options: "
+    f"Choose from options:{Style.RESET_ALL} "
     ).strip().lower()
+
 
     if user_input == 'g':
         length = int(input("Length of password: "))
         password = func.create_pass(length)
-        print(password)
+        print(f"{Fore.GREEN}{Style.BRIGHT}✔ Success! Your Password: {password}{Style.RESET_ALL}")
 
-    # elif first_in == 's':
-    #     if password == '':
-    #         print("Please generate the password first.")
-    #     else:
-    #         file_gen = func.write_pass(password)
-    #         print("Success! Check the file name 'generated_pass.txt' in current directory!")
     
     elif user_input == 'm':
         if password == '':
-            print("Please generate the password first.")
+            print(f"{Fore.RED}✖ Please generate the password first.{Style.RESET_ALL}")
         
         else:
+            name = input("Enter Your name: ")
             site_name = input("Enter the site name or URL: ")
             username = input("Enter username or email: ")
-            creds = func.manage_pass(site_name, username, password)
-            print("Check the 'credential.txt' file.")
+            creds = func.manage_pass(name, site_name, username, password)
+
+            print(f"{Fore.GREEN}{Style.BRIGHT}✔ Success! Check the 'credential.txt' file.{Style.RESET_ALL}")
     
     elif user_input == 'c':
         input_pass = input("Enter your Password: ")
         print(func.password_check(input_pass))
 
+    elif user_input == 's':
+        func.show_my_password()
 
     elif user_input == 'q':
         break
     
     else:
-        print("Invalid input please choose from the options.")
+        print(f"{Fore.RED}{Style.BRIGHT}✖ Invalid input please choose from the options.{Style.RESET_ALL}")
